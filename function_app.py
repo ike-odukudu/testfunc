dd6d49379b03c7a6bca488d0e14dec953c6a443c
ike-odukudu <ike.odukudu@kpmg.co.uk>
2025-04-09 17:03:10 +0100

    Adding SFTP functionality

--- function_app.py
+++ function_app.py
@@ -4,6 +4,7 @@ from azure.storage.blob import BlobServiceClient, generate_blob_sas, BlobSasPerm
 import json
 from datetime import datetime, timedelta
 import os
+import paramiko
 
 
 
@@ -72,6 +73,14 @@ def UploadFileEventTrigger(azeventgrid: func.EventGridEvent):
 
     try:
         event_data = azeventgrid.get_json()
+
+        #Handling Blob Created Events to only use SftpCommit and not SftpCreate
+        api_type = event_data.get("api")
+
+        if api_type != "SftpCommit":
+            logging.info(f"Ignoring event with api: {api_type}")
+            return
+        
         logging.info(f"Event data: {event_data}")
 
         result = json.dumps({
@@ -89,45 +98,81 @@ def UploadFileEventTrigger(azeventgrid: func.EventGridEvent):
         logging.info(f"Blob URL from event: {blob_url}")
 
         # Parse source container and blob name
-        split_url_parts = blob_url.replace("https://", "").split("/", 2)
+        split_url_parts = blob_url.replace("https://", "").split("/", 3)
         source_account_name = split_url_parts[0].split(".")[0]
         source_container_name = split_url_parts[1]
-        source_blob_name = split_url_parts[2]
+        source_container_folder = split_url_parts[2]
+        source_blob_name = split_url_parts[3]
 
         logging.info(f"Storage Account Name: {source_account_name}")
         logging.info(f"Storage Container Name: {source_container_name}")
+        logging.info(f"Storage Container Folder: {source_container_folder}")
         logging.info(f"Storage Blob Name: {source_blob_name}")
 
-        # Destination details
-        destination_container_name = "sftp"
-        destination_blob_name = source_blob_name  # keep same name or customize if needed
+        # SFTP credentials
+        logging.info(f"==================================================")
+        logging.info(f"==================== SFTP LOGIN ==================")
+        sftp_host = os.getenv("SFTP_HOST_NAME")
+        sftp_port = int(os.getenv("SFTP_PORT"))
+        sftp_username = os.getenv("SFTP_USERNAME")  # e.g., "sftpuser1"
+        sftp_password = os.getenv("SFTP_PASSWORD")
 
-        # Construct source blob URL (already from event)
-        source_connection_string = os.getenv("SOURCE_CONNECTION_STRING")
-        source_blob_service = BlobServiceClient.from_connection_string(source_connection_string)
-        source_blob_client = source_blob_service.get_blob_client(container=source_container_name, blob=source_blob_name)
+        remote_file_path = f"./{source_blob_name}"  # this is the virtual path - the parent directory is /sftp/Spotify
+        dest_container_name = "sftp"
+        dest_blob_name = f"{source_container_folder}/{source_blob_name}"
 
-        sas_token = generate_blob_sas(
-            account_name=source_account_name,
-            container_name=source_container_name,
-            blob_name=source_blob_name,
-            account_key=os.getenv("SOURCE_ACCOUNT_KEY"),
-            permission=BlobSasPermissions(read=True),
-            expiry=datetime.utcnow() + timedelta(minutes=10)
-        )
-
-        source_blob_url = f"{blob_url}?{sas_token}"
-        # source_blob_url = blob_url
-
-        # Destination account connection string (hardcoded, since no env var)
+        # Destination blob storage connection
         dest_connection_string = os.getenv("DESTINATION_CONNECTION_STRING")
+        dest_blob_service_client = BlobServiceClient.from_connection_string(dest_connection_string)
+        dest_blob_client = dest_blob_service_client.get_blob_client(container=dest_container_name, blob=dest_blob_name)
 
-        # Copy blob
-        dest_blob_service = BlobServiceClient.from_connection_string(dest_connection_string)
-        dest_blob_client = dest_blob_service.get_blob_client(destination_container_name, destination_blob_name)
-        copy_operation = dest_blob_client.start_copy_from_url(source_blob_url)
+        transport = paramiko.Transport((sftp_host, sftp_port))
+        transport.connect(username=sftp_username, password=sftp_password)
 
-        logging.info(f"Copy operation started. Status: {copy_operation['copy_status']}")
+        sftp = paramiko.SFTPClient.from_transport(transport)
+
+        with sftp.file(remote_file_path, "rb") as file_handle:
+            file_data = file_handle.read()
+            dest_blob_client.upload_blob(file_data, overwrite=True)
+            logging.info("File uploaded to blob successfully.")
+
+        # Delete the file from the SFTP server
+        sftp.remove(remote_file_path)
+        logging.info(f"Deleted source file: {remote_file_path}")
+
+        sftp.close()
+        transport.close()
+
+        # # Destination details
+        # destination_container_name = "sftp"
+        # destination_blob_name = source_blob_name  # keep same name or customize if needed
+
+        # # Construct source blob URL (already from event)
+        # source_connection_string = os.getenv("SOURCE_CONNECTION_STRING")
+        # source_blob_service = BlobServiceClient.from_connection_string(source_connection_string)
+        # source_blob_client = source_blob_service.get_blob_client(container=source_container_name, blob=source_blob_name)
+
+        # sas_token = generate_blob_sas(
+        #     account_name=source_account_name,
+        #     container_name=source_container_name,
+        #     blob_name=source_blob_name,
+        #     account_key=os.getenv("SOURCE_ACCOUNT_KEY"),
+        #     permission=BlobSasPermissions(read=True),
+        #     expiry=datetime.utcnow() + timedelta(minutes=10)
+        # )
+
+        # source_blob_url = f"{blob_url}?{sas_token}"
+        # # source_blob_url = blob_url
+
+        # # Destination account connection string (hardcoded, since no env var)
+        # dest_connection_string = os.getenv("DESTINATION_CONNECTION_STRING")
+
+        # # Copy blob
+        # dest_blob_service = BlobServiceClient.from_connection_string(dest_connection_string)
+        # dest_blob_client = dest_blob_service.get_blob_client(destination_container_name, destination_blob_name)
+        # copy_operation = dest_blob_client.start_copy_from_url(source_blob_url)
+
+        # logging.info(f"Copy operation started. Status: {copy_operation['copy_status']}")
 
     except Exception as e:
         logging.error(f"Error during file copy: {str(e)}")
